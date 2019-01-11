9c778d0dc2e923b5710e3d7b536d9b4d783152e7
TigaxMT <tiagoDeHa@protonmail.com>
2019-01-11 23:42:43 +0000

    Improved output of batch file

--- diswinsvc.bat
+++ diswinsvc.bat
@@ -1,17 +1,28 @@
-echo Starting Disabling Services 
+@echo Starting Disabling Services 
 
-@net stop Fax > null
-@net stop DPS > null
-@net stop Spooler > null
-@net stop seclogon > null
-@net stop PcaSvc > null
-@net stop ScDeviceEnum > null
-@net stop CscService > null
-@net stop stisvc > null
-@net stop DiagTrack > null
-@net stop RemoteRegistry > null
-@net stop WerSvc > null
+@net stop Fax > null 2>&1
+@echo .
+@net stop DPS > null 2>&1
+@echo .
+@net stop Spooler > null 2>&1
+@echo .
+@net stop seclogon > null 2>&1
+@echo .
+@net stop PcaSvc > null 2>&1
+@echo .
+@net stop ScDeviceEnum > null 2>&1
+@echo .
+@net stop CscService > null 2>&1
+@echo .
+@net stop stisvc > null 2>&1
+@echo .
+@net stop DiagTrack > null 2>&1
+@echo .
+@net stop RemoteRegistry > null 2>&1
+@echo .
+@net stop WerSvc > null 2>&1
+@echo .
 
-echo Finished!
+@echo Finished!
 
-PAUSE
+@pause
